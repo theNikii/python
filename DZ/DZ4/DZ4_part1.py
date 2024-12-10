772ab4b1e81b5832221f6910f2a9d5756218b77c
Shaboldin Nikita <Nik.shaboldin@yandex.ru>
2024-12-10 11:15:44 +0300

    dz4

--- DZ/DZ4/DZ4_part1.py
+++ DZ/DZ4/DZ4_part1.py
@@ -1,7 +1,19 @@
+import json
 purchases = {}
 
-f = open('test.txt','r')
+f = open("C:\\Users\\N1k\\python\\DZ\\DZ4\\purchase_log.txt", 'r',encoding='utf-8')
 
-f.readline()
+line = ""
+for line in f:
+    record = json.loads(line)
+    user_id = record.get('user_id')
+    category = record.get('category')
+    if user_id and category:
+            purchases[user_id] = category
+#purchases = f.readlines()
+
+
+print(purchases)
+
 
 f.close()
